67a1b331f780e0fa402a9ad8a69725f70ed16bef
liubingchuan <liubingchuan8@gmail.com>
2019-05-11 09:20:50 +0800

    bb

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -29,7 +29,7 @@ function showLoginInfo(){
 }
 
 function showLoginInfoFrontend(){
-	$.cookie("openId","1");
+	//$.cookie("openId","1");
 	if($.cookie("openId")!=null) {
 		$("#headImg").attr("src", $.cookie("headImg"));
 		if($.cookie("role")!=null && $.cookie("role")==="普通用户"){
